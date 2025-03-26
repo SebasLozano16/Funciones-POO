e109c9df8bf2c41e9f2a83ecc3363f393b13ee2f
SebasLozano16 <1031120217@teinco.edu.co>
2025-03-26 10:40:29 -0500

    Add files via upload

---
+++ Ejemplo.java
@@ -0,0 +1,16 @@
+// Creacion de funcion sumar dos numeros en Java
+public class Ejemplo {
+
+    //Cracion de la funcion
+
+    public static int sumar(int a, int b) {
+        return a + b ;        
+    
+    }
+    
+    // Ejecución de la funcion sumar
+    public static void main(String[] args) {
+        int resultado = sumar(15, 333)
+        System.out.print("resultado");
+    }
+}
